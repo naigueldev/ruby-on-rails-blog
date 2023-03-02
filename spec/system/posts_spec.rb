a742a8dd85d76303ce5a8f17ab7aa1dada363ab0
Naiguel Santos <naiguelsantosdeveloper@gmail.com>
2023-03-02 02:02:28 -0300

    system test create post

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -5,9 +5,29 @@ RSpec.describe "Posts", type: :system do
     driven_by(:rack_test)
   end
 
-  it 'loads the index page with new post link' do
+  it 'loads the index page with new post link text' do
     visit posts_path
 
     expect(page).to have_content('New Post')
+  end
+
+  it 'loads the index page with correct new post link' do
+    visit posts_path
+
+    click_link 'New Post'
+
+    expect(page).to have_content('New Post')
+    expect(page).to have_content('Back')
+  end
+
+
+  it 'load the registered posts' do
+    Post.create(name: 'Test name', title: 'Test title', content: 'Test content')
+    
+    visit posts_path
+
+    expect(page).to have_content('Test name')
+    expect(page).to have_content('Test title')
+    expect(page).to have_content('Test content')
   end
 end
